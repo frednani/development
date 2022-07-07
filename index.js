0edd3353795b4aba1df3f28724788749a8c24722
Karangwa Fred <45320487+frednani@users.noreply.github.com>
2022-07-07 11:14:05 +0000

    pig Latin challenge

--- index.js
+++ index.js
@@ -332,20 +332,33 @@
 
 
 // seek and destroy coding challenge//
-function destroyer(arr) {
-  const valsToRemove = Object.values(arguments).slice(1);
-  const filteredArray = [];
+// function destroyer(arr) {
+//   const valsToRemove = Object.values(arguments).slice(1);
+//   const filteredArray = [];
 
-  for (let i = 0; i < arr.length; i++) {
-    let removeElement = false;
-    for (let j = 0; j < valsToRemove.length; j++) {
-      if (arr[i] === valsToRemove[j]) {
-        removeElement = true;
-      }
-    }
-    if (!removeElement) {
-      filteredArray.push(arr[i]);
-    }
-  }
-  return filteredArray;
+//   for (let i = 0; i < arr.length; i++) {
+//     let removeElement = false;
+//     for (let j = 0; j < valsToRemove.length; j++) {
+//       if (arr[i] === valsToRemove[j]) {
+//         removeElement = true;
+//       }
+//     }
+//     if (!removeElement) {
+//       filteredArray.push(arr[i]);
+//     }
+//   }
+//   return filteredArray;
+// }
+//pig latin answer!! challenge//
+function translatePigLatin(str) {
+  let consonantRegex = /^[^aeiou]+/;
+  let myConsonants = str.match(consonantRegex);
+  return myConsonants !== null
+    ? str
+        .replace(consonantRegex, "")
+        .concat(myConsonants)
+        .concat("ay")
+    : str.concat("way");
 }
+
+translatePigLatin("consonant");
