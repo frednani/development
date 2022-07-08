6abb59711402e8944954794f2ca75bc35b123697
Karangwa Fred <45320487+frednani@users.noreply.github.com>
2022-07-08 13:04:34 +0000

    prime number and sum

--- index.js
+++ index.js
@@ -429,3 +429,22 @@ function convertHTML(str) {
 
 //test here
 convertHTML("Dolce & Gabbana");
+// challenge of finding prime number and calculate thier sum //
+function sumPrimes(num) {
+  // Helper function to check primality
+  function isPrime(num) {
+    for (let i = 2; i <= Math.sqrt(num); i++) {
+      if (num % i == 0)
+        return false;
+    }
+    return true;
+  }
+
+  // Check all numbers for primality
+  let sum = 0;
+  for (let i = 2; i <= num; i++) {
+    if (isPrime(i))
+      sum += i;
+  }
+  return sum;
+}
