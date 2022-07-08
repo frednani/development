b28a270463dfa8bc6d37f89168918bdc3826b5be
Karangwa Fred <45320487+frednani@users.noreply.github.com>
2022-07-08 12:08:39 +0000

    solving html convertion challenge

--- index.js
+++ index.js
@@ -350,47 +350,82 @@
 //   return filteredArray;
 // }
 //pig latin answer!! challenge//
-function translatePigLatin(str) {
-  let consonantRegex = /^[^aeiou]+/;
-  let myConsonants = str.match(consonantRegex);
-  return myConsonants !== null
-    ? str
-        .replace(consonantRegex, "")
-        .concat(myConsonants)
-        .concat("ay")
-    : str.concat("way");
-}
+// function translatePigLatin(str) {
+//   let consonantRegex = /^[^aeiou]+/;
+//   let myConsonants = str.match(consonantRegex);
+//   return myConsonants !== null
+//     ? str
+//         .replace(consonantRegex, "")
+//         .concat(myConsonants)
+//         .concat("ay")
+//     : str.concat("way");
+// }
 
-translatePigLatin("consonant");
-function pairElement(str) {
-  // Return each strand as an array of two elements, the original and the pair.
-  var paired = [];
+// translatePigLatin("consonant");
+// function pairElement(str) {
+//   // Return each strand as an array of two elements, the original and the pair.
+//   var paired = [];
 
-  // Function to check with strand to pair.
-  var search = function(char) {
-    switch (char) {
-      case "A":
-        paired.push(["A", "T"]);
-        break;
-      case "T":
-        paired.push(["T", "A"]);
-        break;
-      case "C":
-        paired.push(["C", "G"]);
-        break;
-      case "G":
-        paired.push(["G", "C"]);
-        break;
-    }
-  };
+//   // Function to check with strand to pair.
+//   var search = function(char) {
+//     switch (char) {
+//       case "A":
+//         paired.push(["A", "T"]);
+//         break;
+//       case "T":
+//         paired.push(["T", "A"]);
+//         break;
+//       case "C":
+//         paired.push(["C", "G"]);
+//         break;
+//       case "G":
+//         paired.push(["G", "C"]);
+//         break;
+//     }
+//   };
 
-  // Loops through the input and pair.
-  for (var i = 0; i < str.length; i++) {
-    search(str[i]);
-  }
+//   // Loops through the input and pair.
+//   for (var i = 0; i < str.length; i++) {
+//     search(str[i]);
+//   }
 
-  return paired;
-}
+//   return paired;
+// }
 
 // test here
-pairElement("GCG");
+// pairElement("GCG");
+
+// level 5 challenge Convert HTML Entities
+function convertHTML(str) {
+  // Split by character to avoid problems.
+
+  let temp = str.split("");
+
+  // Since we are only checking for a few HTML elements, use a switch
+
+  for (let i = 0; i < temp.length; i++) {
+    switch (temp[i]) {
+      case "<":
+        temp[i] = "&lt;";
+        break;
+      case "&":
+        temp[i] = "&amp;";
+        break;
+      case ">":
+        temp[i] = "&gt;";
+        break;
+      case '"':
+        temp[i] = "&quot;";
+        break;
+      case "'":
+        temp[i] = "&apos;";
+        break;
+    }
+  }
+
+  temp = temp.join("");
+  return temp;
+}
+
+//test here
+convertHTML("Dolce & Gabbana");
