6ef0f497dc3c36ec3fa24475ca8c80c35c51827e
Karangwa Fred <45320487+frednani@users.noreply.github.com>
2022-08-28 14:31:11 +0000

    added orange and apples challenges

--- index.js
+++ index.js
@@ -516,4 +516,23 @@ function simpleArraySum(ar) {
   return sum;
 }
 
-console.log("hello saidi");
+// hacker rank challenges  orange and apples !!
+function countApplesAndOranges(s, t, a, b, apples, oranges) {
+  const fLoc = function(treeLoc, arr2d) {
+    return arr2d.map(fruitLoc => (treeLoc + fruitLoc));
+  }
+  const fRange = function(s, t, arr2d) {
+    let a, b;
+    a = 0; b = 0;
+    arr2d.forEach((f, i) => {
+      if (i === 0) { // apple count
+        f.forEach(loc => s <= loc && loc <= t ? a++ : null);
+      }
+      if (i === 1) { // orange count
+        f.forEach(loc => s <= loc && loc <= t ? b++ : null);
+      }
+    });
+    return [a, b];
+  }
+  console.log(fRange(s, t, [fLoc(a, apples), fLoc(b, oranges)]).join('\n'));
+}
